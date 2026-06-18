e57a94579f5afad8fbf066c5e65747cd9dc25970
neomaid <me@maid.dev>
2026-06-18 12:22:40 +0200

    pkgs/helium: 0.12.5.1 -> 0.13.3.1

--- pkgs/helium.nix
+++ pkgs/helium.nix
@@ -8,11 +8,11 @@
 }:
 let
   pname = "helium";
-  version = "0.12.5.1";
+  version = "0.13.3.1";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    sha256 = "sha256-uUZauNralX6katmnO9VDLEs+d+HIhkjkeV36Dw2eUmM=";
+    sha256 = "sha256-RS+Sn42V+HjCw41N1zayMVIqlgH+i2B2IdVJwBPmw00=";
   };
 
   desktopItem = makeDesktopItem {
